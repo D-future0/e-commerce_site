bfa6ea68e59e87d2d7f55d1b88e72abe5bbfde35
D-future0 <oka4collins@gmail.com>
2023-08-08 03:31:35 +0200

    dynamically upload items

--- setup/index.js
+++ setup/index.js
@@ -1,3 +1,9 @@
+const items = [
+    {imageSrc:`"/E-com-web-img/phoneDir/tecnoCamon18.jpg"`, itemName:`tecno Camon18`, itemPrice:`97000`, productName:`tecno`, itemCategory:`phone`, itemSubCategory:`andriod`, expressDelivery:`True`, promo:`True`},
+    {imageSrc:`"/E-com-web-img/phoneDir/tecnoSpark10.jpg"`, itemName:`tecno Spark10`, itemPrice:`65000`, productName:`tecno`, itemCategory:`phone`, itemSubCategory:`andriod`, expressDelivery:`False`, promo:`True`},
+    // {imageSrc:, itemName:, itemPrice:, productName:, itemCategory:, itemSubCategory:, expressDelivery:, promo:,},
+]
+
 /* search toggle*/
 const searchIcon = document.querySelector(`.search_icon`);
 const searchBar = document.querySelector(`.searchBar2`);
@@ -24,7 +30,7 @@ advanceMenu.addEventListener(`click`, (event)=>{
             activeTab.classList.add(`active`) 
         } 
 })
-advanceMenu.addEventListener(`dblclick`, (event)=>{
+advanceMenu.addEventListener(`dblclick`, ()=>{
             tabs.forEach((tab)=>{
             tab.classList.remove(`active`);
             });
@@ -32,3 +38,57 @@ advanceMenu.addEventListener(`dblclick`, (event)=>{
                 opt.classList.remove(`active`);
             });
         });
+
+        /** display setup **/
+ const expressDelivery = document.querySelector(`.expressDelivery`);
+ const promotions = document.querySelector(`.promotions`);
+ 
+window.addEventListener(`DOMContentLoaded`, function (){
+    displayAllExpressDeliveryItems(items)
+    displayAllPromotionItems(items)
+ });
+
+ /** functions **/
+//  functions all items function
+let displayAllItems = (allItems, category) => {
+    displayItem = allItems.map((item) => {
+        return `<div class="card-container">
+        <article class="card">
+            <div class="img-container">
+                <img src=${item.imageSrc} alt="tecnoCamon18" class="item-img">
+            </div>
+            <div class="info-container">
+                <p class="item-name">${item.itemName}</p>
+                <p class="item-price">${item.itemPrice}N</p>
+            </div>
+            <div class="button-container">
+                <button type="submit" class="btn">Add To Cart</button>
+            </div>
+        </article>
+    </div>
+</div>`
+})
+    displayItem = displayItem.join(``)
+    category.innerHTML = displayItem
+
+};
+// display express delivery function
+function displayAllExpressDeliveryItems() {
+    let express= `True`
+    displayexpressItems = items.filter((item) => {
+        if (item.expressDelivery===express) {
+            return item
+        }
+    })
+    displayAllItems(displayexpressItems, expressDelivery)
+}
+// display express delivery function
+function displayAllPromotionItems() {
+    let promos= `True`
+    displayPromoItems = items.filter((item) => {
+        if (item.promo===promos) {
+            return item
+        }
+    })
+    displayAllItems(displayPromoItems, promotions)
+}
